9749f4bc18c2a689097fbf9abe708415d7fbb796
Ratnadeep Simhadri <rsimhadri@paypal.com>
2022-08-15 17:27:21 -0700

    chore: add only minified version for lib

--- webpack.config.js
+++ webpack.config.js
@@ -11,17 +11,10 @@ const MODULE_NAME = 'paypal-legal-components';
 
 export const WEBPACK_CONFIG = getWebpackConfig({
     filename:   `${ FILE_NAME }.js`,
-    modulename: MODULE_NAME
-});
-
-export const WEBPACK_CONFIG_MIN = getWebpackConfig({
-    filename:   `${ FILE_NAME }.min.js`,
     modulename: MODULE_NAME,
     minify:     true,
     vars:       {
         __MIN__: true
     }
 });
-
-
-export default [ WEBPACK_CONFIG, WEBPACK_CONFIG_MIN ]; // eslint-disable-line import/no-default-export
+export default [ WEBPACK_CONFIG ]; // eslint-disable-line import/no-default-export
